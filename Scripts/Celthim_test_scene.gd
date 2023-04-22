9b586765de6d0f68f35931409629b0dc5472abfb
Celthim <34304819+Celthim@users.noreply.github.com>
2023-04-22 15:07:07 +0200

    Slenderman Ok

--- Scripts/Celthim_test_scene.gd
+++ Scripts/Celthim_test_scene.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Slenderman.play(Slenderman.IDLE)
+	$Slenderman.play(Slenderman.RUN)
 #	$Idle/AnimationPlayer.play("mixamocom")
 	pass # Replace with function body.
 
@@ -13,5 +15,6 @@ func _process(delta):
 
 
 func _on_animation_player_animation_finished(anim_name):
+	return
 	$Idle/AnimationPlayer.play("mixamocom")
 	pass # Replace with function body.
