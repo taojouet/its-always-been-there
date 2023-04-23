fd3cdd18868a12d8588e0b41046443891354e8d2
Massi <maximiliano.pinna@gmail.com>
2023-04-23 03:29:40 +0200

    Condition de défaites runner

--- Assets/Slenderman/Slenderman.gd
+++ Assets/Slenderman/Slenderman.gd
@@ -3,7 +3,7 @@ class_name Slenderman
 
 signal slenderman_catch
 
-var speed = 5.0
+var speed = 6.0
 
 enum {
 	IDLE,
@@ -11,14 +11,21 @@ enum {
 	DRINK
 }
 
+var is_active = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	play(RUN)
 
+func activate(active):
+	is_active = active
+	visible = active
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	translate(Vector3(0, 0, speed * delta))
+	if is_active:
+		translate(Vector3(0, 0, speed * delta))
 
 func hide_anim():
 	for a in get_children():
@@ -34,7 +41,9 @@ func play(anim):
 		DRINK:
 			$Drink.visible = true
 
-func _on_slendermandetection_body_entered(body):
-	if body is Player:
+
+func _on_area_3d_body_entered(body):
+	if body is Player and is_active:
 		emit_signal("slenderman_catch")
+		print("CATCH")
 	pass # Replace with function body.
