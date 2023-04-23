8709d9b270ce12239c7bcc36e43e9371e0500dc9
Massi <maximiliano.pinna@gmail.com>
2023-04-23 02:29:53 +0200

    Slender Rune

--- Assets/Slenderman/Slenderman.gd
+++ Assets/Slenderman/Slenderman.gd
@@ -1,6 +1,9 @@
 extends Node3D
 class_name Slenderman
 
+signal slenderman_catch
+
+var speed = 5.0
 
 enum {
 	IDLE,
@@ -11,13 +14,11 @@ enum {
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	play(IDLE)
-
+	play(RUN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	translate(Vector3(0, 0, speed * delta))
 
 func hide_anim():
 	for a in get_children():
@@ -32,4 +33,8 @@ func play(anim):
 			$Run.visible = true
 		DRINK:
 			$Drink.visible = true
-			
+
+func _on_slendermandetection_body_entered(body):
+	if body is Player:
+		emit_signal("slenderman_catch")
+	pass # Replace with function body.
--- Scenes/Runner.gd
+++ Scenes/Runner.gd
@@ -5,9 +5,12 @@ const PLATEFORM_SCALE = 5
 
 @onready var plateform = $Plateform
 const plateform_path = preload("res://Scripts/Plateform.tscn")
-const NUM_PLATFORMS = 2
+const NUM_PLATFORMS = 15
 var plateforms = []
 
+var player_start_pos_x
+var player_start_pos_y
+var player_start_pos_z
 		
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,18 +19,23 @@ func _ready():
 		var p = plateform_path.instantiate()
 		add_child(p)
 		p.scale *= PLATEFORM_SCALE
-#		p.getPlayerdetection().connect("body_entered", movePlateform)
+
 		p.connect("player_entered",movePlateform)
 		plateforms.append(p)
 		p.translate(Vector3(0, 0, i*TILE_SIZE))
+		if i<round(NUM_PLATFORMS/2):
+			p.can_trigger = false
 		if i == round(NUM_PLATFORMS/2):
 			$Player.global_position.x=p.global_position.x
 			$Player.global_position.z=p.global_position.z
-		
-		if i == round(NUM_PLATFORMS/2-2):
+			player_start_pos_x = $Player.global_position.x
+			player_start_pos_z = $Player.global_position.z
+			
+		if i == round(NUM_PLATFORMS/2):
 			$Slenderman.global_position.x=p.global_position.x
 			$Slenderman.global_position.z=p.global_position.z
 		
+		$Slenderman.connect("slenderman_catch", death)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -37,9 +45,6 @@ func movePlateform():
 	plateforms[0].rand_forest()
 	plateforms.append(plateforms.pop_front())
 
-#func movePlateform(body):
-#	return
-#	if body is Player:
-#		plateforms[0].translate( Vector3(0,0,len(plateforms)*TILE_SIZE ) )
-#		plateforms[0].rand_forest()
-#		plateforms.append(plateforms.pop_front())
+func death():
+	$Player.global_position.x = player_start_pos_x
+	$Player.global_position.z = player_start_pos_z
